7115020aa1f3123622543e0d2539dba4df0a9730
Arjun S <arjun.s54321@mail.com>
2025-05-07 23:02:29 +0530

    Removed banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Services from './pages/Services';
@@ -13,7 +12,6 @@ function App() {
     <Router>
       <div className="min-h-screen flex flex-col">
         <Navbar />
-        <Banner />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
