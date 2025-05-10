cb7dea8ad8d89912bad096b17068550e891f0c54
Ramees Muhammed <rameesmuhammed957@gmail.com>
2025-05-10 15:45:20 +0530

    Social media links added in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div>
-            <img className='w-[50px]' src="../public/images/MB-White-Logo.png" alt="" />
+            <img className='w-[100px]' src="../public/images/MB-White-Logo.png" alt="" />
           </div>
           <div>
             <h4 className="text-lg font-semibold mb-4">Quick Links</h4>
@@ -28,14 +28,29 @@ const Footer = () => {
           <div>
             <h4 className="text-lg font-semibold mb-4">Connect With Us</h4>
             <div className="flex space-x-4">
-              <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                <Facebook className="h-6 w-6" />
+              <a
+                href="https://www.facebook.com/share/1BwtX9z58R/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-red-600 hover:bg-red-700 transition p-3 rounded-full"
+              >
+                <Facebook className="w-5 h-5" />
               </a>
-              <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                <Instagram className="h-6 w-6" />
+              <a
+                href="https://www.instagram.com/medobyte.tech?igsh=MTNlcDR3eHl5MTZhcw=="
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-red-600 hover:bg-red-700 transition p-3 rounded-full"
+              >
+                <Instagram className="w-5 h-5" />
               </a>
-              <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                <Linkedin className="h-6 w-6" />
+              <a
+                href="https://www.linkedin.com/in/medobyte-technologies?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-red-600 hover:bg-red-700 transition p-3 rounded-full"
+              >
+                <Linkedin className="w-5 h-5" />
               </a>
             </div>
           </div>
