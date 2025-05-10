00e099421f4866197085d93210cdccdbd619b6a4
subhashosSurendran <subhashos28@gmail.com>
2025-05-10 14:41:59 +0530

    add logo image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div>
-            <img className='w-[50px]' src="../public/images/MB-White-Logo.png" alt="" />
+            <img className='w-[50px]' src="/images/MB-White-Logo.png" alt="" />
             <p className="text-gray-400">
               Transforming businesses through innovative e-commerce solutions and digital marketing strategies.
             </p>
