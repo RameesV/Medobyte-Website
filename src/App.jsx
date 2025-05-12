7b7a5b9e3e3573e14abc9be0f5843a4791aa3a2f
Ramees Muhammed <50680076+RameesV@users.noreply.github.com>
2025-05-13 00:52:53 +0530

    Contact form added (#10)
    * Contact form working
    
    * react-hot-toast library added

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,28 @@ import Services from './pages/Services';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import { HelmetProvider } from 'react-helmet-async';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <HelmetProvider>
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+      <Router>
+        {/* ✅ Global Toaster component */}
+        <Toaster position="top-center" reverseOrder={false} />
+        
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
     </HelmetProvider>
   );
 }
